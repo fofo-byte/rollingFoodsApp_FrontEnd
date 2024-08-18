d9632672307d64064d22f1d2641df89c66d70404
alfonso <alfonso.casucci@gmail.com>
2024-08-18 11:31:57 +0200

    updated

--- lib/screens/pageCreateGround.dart
+++ lib/screens/pageCreateGround.dart
@@ -27,13 +27,53 @@ class CreateGround extends StatelessWidget {
               letterSpacing: 2.0),
         ),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text("Page de création de lieux d'emplacements"),
-              ElevatedButton(
+              const Text("Page de création de lieux d'emplacements"),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  counterStyle: TextStyle(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  labelText: 'Nom du lieu',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  counterStyle: TextStyle(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  labelText: 'Adresse',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                maxLines: 3,
+                decoration: const InputDecoration(
+                  counterStyle: TextStyle(),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                  labelText: 'Description',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const ElevatedButton(
                 onPressed: null,
                 child: Text('Créer un lieux d\'emplacement'),
               ),
